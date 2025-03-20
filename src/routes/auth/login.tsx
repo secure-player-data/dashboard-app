cbefa7fd0ec75ffff5ba589f92217bc57192d546
Joakim Edvardsen <77448888+jKm00@users.noreply.github.com>
2025-03-20 13:28:26 +0100

    fix: remove igrant button

--- src/routes/auth/login.tsx
+++ src/routes/auth/login.tsx
@@ -13,7 +13,7 @@ export const Route = createFileRoute('/auth/login')({
 
 function RouteComponent() {
   return (
-    <div className="flex min-h-svh flex-col items-center justify-center gap-6 bg-muted p-6 md:p-10">
+    <div className="flex min-h-svh flex-col items-center justify-center bg-muted dark:bg-neutral-900 gap-6 p-6 md:p-10">
       <div className="flex w-full max-w-sm flex-col gap-6">
         <LoginForm />
       </div>

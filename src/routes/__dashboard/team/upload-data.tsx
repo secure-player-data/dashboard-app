4f654ae6209fbcc2a9d6bbc01009b704112d00d2
TorsteinEide <77608436+TorsteinEide@users.noreply.github.com>
2025-03-26 13:17:11 +0100

    init

--- src/routes/__dashboard/team/upload-data.tsx
+++ src/routes/__dashboard/team/upload-data.tsx
@@ -1,9 +1,10 @@
-import { createFileRoute } from '@tanstack/react-router'
+import { createFileRoute } from '@tanstack/react-router';
+import UploadDataForm from '@/components/forms/upload-data-form';
 
 export const Route = createFileRoute('/__dashboard/team/upload-data')({
   component: RouteComponent,
-})
+});
 
 function RouteComponent() {
-  return <div>Hello "/__dashboard/upload-data"!</div>
+  return <UploadDataForm />;
 }

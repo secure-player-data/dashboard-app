47ad35bb764407dfd12e8b2bc0b85551ed09874a
Joakim Edvardsen <77448888+jKm00@users.noreply.github.com>
2025-03-26 11:13:59 +0100

    SPD 239 More code to remove
    * chore: remove deprecated code from version 0.2
    
    * chore: remove old seeding
    
    * chore: remove old schemas

--- src/schemas/tracking-data.ts
+++
@@ -1,7 +0,0 @@
-export const TRACKING_DATA_SCHEMA = {
-  type: 'https://schema.org/TrackingData',
-  coordinates: 'https://schema.org/coordinates',
-  time: 'https://schema.org/time',
-  speed: 'https://schema.org/speed',
-  distance: 'https://schema.org/distance',
-};
